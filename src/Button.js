f653ffdc672e950ce05373fddf7bb7a3f52fa5cd
Sanjay Agnani <sanjayagnani@crikle.com>
2024-06-28 18:30:51 +0530

    add console logs

--- src/Button.js
+++ src/Button.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Button.css';
 
 const Button = () => {
+  const [num, setNum] = React.useState(10);
+  console.log("num", num);
+  
   return (
     <button className="bottom-right-button">
       Connect with Me
