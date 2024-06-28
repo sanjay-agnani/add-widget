f3e3238232b3da3d584e2f525d5cd22b7911c94c
Sanjay Agnani <sanjayagnani@crikle.com>
2024-06-28 17:08:19 +0530

    First Commit

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+  return (
+    <button className="bottom-right-button">
+      Connect with Me
+    </button>
+  );
+};
+
+export default Button;
